23785b4c7fb1a81601c26309b58c9b0e74bd7443
NjilaBoris <njilaboris2@gmail.com>
2025-05-16 01:22:05 +0100

    MobileNav and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,7 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
   return (
     <>
       <h1 className="h1-bold">Welcome to Nwxtjs is the best course</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 };
